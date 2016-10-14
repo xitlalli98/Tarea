c163d72952e2b301a7aab3f39beeca8839fe384f
Xitlalli Ruiz <Xitlalli Ruiz>
2016-10-13 18:28:17 -0700

    xitlalli

---
+++ Visual Studio 2015/Projects/regina/salario/Tarea/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona _persona = _persona = new Persona();
+            _persona.Inicializar();
+            _persona.Impuestos();
+            _persona.Imprimir();
+            
+        }
+    }
+}
